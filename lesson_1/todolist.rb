8d392c79ff2585c0fd5d653a016098feceb941b6
Nicolas Lacressonniere <nicolas.lacressonniere@gmail.com>
2018-06-26 22:15:53 +0200

    TodoList assignment

--- lesson_1/todolist.rb
+++ lesson_1/todolist.rb
@@ -45,42 +45,56 @@ class TodoList
 
   # rest of class needs implementation
   def add(todo)
+    raise TypeError, "Can only add Todo objects" unless todo.instance_of?(Todo)
+
+    @todos << todo
   end
-  
+
+  def <<(todo)
+    add(todo)
+  end
+
   def size
+    @todos.size
   end
-  
+
   def first
+    @todos.first
   end
-  
+
   def last
+    @todos.last
   end
-  
+
   def item_at(index)
-    
+    @todos.fetch(index)
   end
 
   def mark_done_at(index)
-    
+    item_at(index).done!
   end
 
   def mark_undone_at(index)
-    
+    item_at(index).undone!
   end
 
   def remove_at(index)
-    
+    @todos.delete(item_at(index))
   end
-  
+
   def shift
+    @todos.shift
   end
-  
+
   def pop
+    @todos.pop
   end
-  
+
   def to_s
+    str = "---- #{title} ----\n\r"
+    @todos.each { |todo| str += "#{todo}\n\r" }
+    str
   end
-  
 end
 
 # given
@@ -89,123 +103,89 @@ todo2 = Todo.new("Clean room")
 todo3 = Todo.new("Go to gym")
 list = TodoList.new("Today's Todos")
 
-# ---- Adding to the list -----
+puts "---- Adding to the list -----"
 
 # add
-list.add(todo1)                 # adds todo1 to end of list, returns list
-list.add(todo2)                 # adds todo2 to end of list, returns list
-list.add(todo3)                 # adds todo3 to end of list, returns list
+p list.add(todo1)                 # adds todo1 to end of list, returns list
+p list.add(todo2)                 # adds todo2 to end of list, returns list
+p list.add(todo3)                 # adds todo3 to end of list, returns list
 begin
-  list.add(1)                   # raises TypeError with message "Can only add Todo objects"
+  list.add(1) # raises TypeError with message "Can only add Todo objects"
   puts false
-rescue TypeError => e
-  puts "#{e.message}"
+rescue TypeError
   puts true
 end
 
 # <<
 # same behavior as add
 
-# ---- Interrogating the list -----
+puts "---- Interrogating the list -----"
 
 # size
-list.size                       # returns 3
+p list.size == 3 # returns 3
 
 # first
-list.first                      # returns todo1, which is the first item in the list
+p list.first.equal?(todo1) # returns todo1, which is the first item in the list
 
 # last
-list.last                       # returns todo3, which is the last item in the list
+p list.last.equal?(todo3) # returns todo3, which is the last item in the list
 
-# ---- Retrieving an item in the list ----
+puts "---- Retrieving an item in the list ----"
 
 # item_at
 begin
   list.item_at                  # raises ArgumentError
   puts false
-rescue ArgumentError => e
-  puts "#{e.message}"
+rescue ArgumentError
   puts true
 end
 
-list.item_at(1)                 # returns 2nd item in list (zero based index)
+p list.item_at(1)               # returns 2nd item in list (zero based index)
 begin
   list.item_at(100)             # raises IndexError
   puts false
-rescue IndexError => e
-  puts "#{e.message}"
+rescue IndexError
   puts true
 end
 
+puts "---- Marking items in the list -----"
 
-# ---- Marking items in the list -----
-
-# mark_done_at
+puts "mark_done_at"
 begin
   list.mark_done_at             # raises ArgumentError
   puts false
-rescue ArgumentError => e
-  puts "#{e.message}"
+rescue ArgumentError
   puts true
 end
 
-list.mark_done_at(1)            # marks the 2nd item as done
+p list.mark_done_at(1)          # marks the 2nd item as done
 begin
   list.mark_done_at(100)        # raises IndexError
   puts false
-rescue IndexError => e
-  puts "#{e.message}"
+rescue IndexError
   puts true
 end
 
-# mark_undone_at
+puts "mark_undone_at"
 begin
-  list.mark_undone_at            # raises ArgumentError
+  list.mark_undone_at           # raises ArgumentError
   puts false
-rescue ArgumentError => e
-  puts "#{e.message}"
+rescue ArgumentError
   puts true
 end
 
-list.mark_undone_at(1)          # marks the 2nd item as not done,
+p list.mark_undone_at(1) == false   # marks the 2nd item as not done,
 begin
-  list.mark_undone_at(100)      # raises IndexError
+  list.mark_undone_at(100)          # raises IndexError
   puts false
-rescue IndexError => e
-  puts "#{e.message}"
+rescue IndexError
   puts true
 end
 
-# ---- Deleting from the the list -----
-
-# shift
-list.shift                      # removes and returns the first item in list
-
-# pop
-list.pop                        # removes and returns the last item in list
-
-# remove_at
-begin
-  list.remove_at                # raises ArgumentError
-  puts false
-rescue ArgumentError => e
-  puts "#{e.message}"
-  puts true
-end
-
-list.remove_at(1)               # removes and returns the 2nd item
-begin
-  list.remove_at(100)           # raises IndexError
-  puts false
-rescue IndexError => e
-  puts "#{e.message}"
-  puts true
-end
-
-# ---- Outputting the list -----
+puts "---- Outputting the list -----"
 
 # to_s
-list.to_s                      # returns string representation of the list
+puts list # returns string representation of the list
 
 # ---- Today's Todos ----
 # [ ] Buy milk
@@ -218,3 +198,27 @@ list.to_s                      # returns string representation of the list
 # [ ] Buy milk
 # [X] Clean room
 # [ ] Go to gym
+
+puts "---- Deleting from the the list -----"
+
+puts "remove_at"
+begin
+  list.remove_at                  # raises ArgumentError
+  puts false
+rescue ArgumentError
+  puts true
+end
+
+p list.remove_at(1)               # removes and returns the 2nd item
+begin
+  list.remove_at(100)             # raises IndexError
+  puts false
+rescue IndexError
+  puts true
+end
+
+puts "shift"
+p list.shift                      # removes and returns the first item in list
+
+puts "pop"
+p list.pop                        # removes and returns the last item in list
