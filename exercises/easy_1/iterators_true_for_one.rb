3f808bf65c882e98f69009633f6015859384046e
Nicolas Lacressonniere <nicolas.lacressonniere@gmail.com>
2018-07-03 22:36:33 +0200

    Add Iterators: True for One? exercise

---
+++ exercises/easy_1/iterators_true_for_one.rb
@@ -0,0 +1,18 @@
+def one?(collection)
+  result = false
+  collection.each do |item|
+    if yield(item)
+      return false if result
+      result = true
+    end
+  end
+  result
+end
+
+p one?([1, 3, 5, 6]) { |value| value.even? }    # -> true
+p one?([1, 3, 5, 7]) { |value| value.odd? }     # -> false
+p one?([2, 4, 6, 8]) { |value| value.even? }    # -> false
+p one?([1, 3, 5, 7]) { |value| value % 5 == 0 } # -> true
+p one?([1, 3, 5, 7]) { |value| true }           # -> false
+p one?([1, 3, 5, 7]) { |value| false }          # -> false
+p one?([]) { |value| true }                     # -> false
